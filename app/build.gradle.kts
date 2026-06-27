a16ebc0a56e9a3dc08938ffb87aae52085e95d64
jibinkp <jibinkp770@gmail.com>
2026-06-27 11:02:03 +0530

    firebase connectivity, implemented user register and login using email and password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -42,8 +43,14 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.firebase.auth)
+    implementation(libs.googleid)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    implementation(libs.kotlinx.coroutines.android)
 }
--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.firebaseauth
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebaseauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +28,80 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // initialize the firebase auth
+        auth = FirebaseAuth.getInstance()
+        // logout the user force fully if the user is logged in
+        auth.signOut()
+
+        binding.btnRegister.setOnClickListener {
+            registerUser()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedState()
+    }
+
+    /**
+     * Function to register user using firebase createUserWithEmailAndPassword function
+     * Then call function checkLoggedState to set the UI as per user logged in/not
+     * */
+    private fun registerUser() {
+        val email = binding.etRegEmail.text.toString()
+        val password = binding.etRegPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email,password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedState()
+                    }
+                }catch (e: Exception) {
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Function to log-in user using firebase signInWithEmailAndPassword function
+     * Then call function checkLoggedState to set the UI as per user logged in/not
+     * */
+    private fun loginUser() {
+        val email = binding.etLoginEmail.text.toString()
+        val password = binding.etLoginPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(email,password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedState()
+                    }
+                }catch (e: Exception) {
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Function to set the UI of logged in/out user using firebase authenticator
+     * */
+    private fun checkLoggedState() {
+        if (auth.currentUser == null) {
+            binding.tvLoggedStatus.text = "you are not logged in"
+        } else {
+            binding.tvLoggedStatus.text = "You are logged in"
         }
     }
 }
